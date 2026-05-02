613c2fb367135a35752ff7031832535b7d100737
Marco Antonio Bautista <157225804+txniii@users.noreply.github.com>
2026-05-02 14:31:43 -0400

    Change document root to current directory for PHP server
    Updated document root for PHP built-in server.

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Start PHP built-in server for Render.com
-# Render sets the PORT environment variable automatically
-php -S 0.0.0.0:${PORT:-10000} -t /var/www/html router.php
+php -S 0.0.0.0:${PORT:-10000} -t . router.php
