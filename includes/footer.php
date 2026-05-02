5109e9135daff0731ac712b6cbe522e6e6d61ba2
Marco Antonio Bautista <157225804+txniii@users.noreply.github.com>
2026-05-02 12:47:53 -0400

    Update footer with new content and links

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,10 @@
-<footer>
-    <p>&copy; 2026 My Website. All rights reserved.</p>
-    <p>Last updated on 2026-05-02 16:03:43 UTC.</p>
-</footer>
+    </main>
+
+    <footer class="footer">
+        <div class="container">
+            <p>&copy; 2026 F1 Planner. All rights reserved. Marco Bautista | <a href="/index.php">Home</a> | <a href="https://github.com/txniii/f1-planner">GitHub</a></p>
+        </div>
+    </footer>
+    <script src="/public/js/app.js"></script>
+</body>
+</html>
