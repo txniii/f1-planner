47623b506b67a155154f1f32539d7143b9ad2aa8
Marco Antonio Bautista <157225804+txniii@users.noreply.github.com>
2026-05-08 22:54:11 -0400

    Update password for admin user
    Update admin user password in the database.

--- trimmed-database.sql
+++ trimmed-database.sql
@@ -116,3 +116,7 @@ CREATE TABLE IF NOT EXISTS user_races (
 SELECT id, username, email, password_hash, role
 FROM users
 WHERE email = 'admin@f1planner.com';
+
+UPDATE users
+SET password_hash = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+WHERE email = 'admin@f1planner.com';
